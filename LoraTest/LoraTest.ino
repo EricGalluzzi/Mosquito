dc6bb230e79c969f5f89b95107996fc7877e22e0
Eric <ericbriangall2004@gmail.com>
2022-01-03 13:51:06 -0500

    Eric's Esp encantor

---
+++ LoraTest/LoraTest.ino
@@ -0,0 +1,182 @@
+ 
+/*
+ * HelTec Automation(TM) LoRaWAN 1.0.2 OTAA example use OTAA, CLASS A
+ *
+ * Function summary:
+ *
+ * - use internal RTC(150KHz);
+ *
+ * - Include stop mode and deep sleep mode;
+ *
+ * - 15S data send cycle;
+ *
+ * - Informations output via serial(115200);
+ *
+ * - Only ESP32 + LoRa series boards can use this library, need a license
+ *   to make the code run(check you license here: http://www.heltec.cn/search );
+ *
+ * You can change some definition in "Commissioning.h" and "LoRaMac-definitions.h"
+ *
+ * HelTec AutoMation, Chengdu, China.
+ * 成都惠利特自动化科技有限公司
+ * https://heltec.org
+ * support@heltec.cn
+ *
+ *this project also release in GitHub:
+ *https://github.com/HelTecAutomation/ESP32_LoRaWAN
+*/
+
+#include <ESP32_LoRaWAN.h>
+#include <ArduinoJson.h>  
+#include "Arduino.h"
+
+/*license for Heltec ESP32 LoRaWan, quary your ChipID relevant license: http://resource.heltec.cn/search */
+uint32_t  license[4] = {0x4078715A,0x9A528C5D,0x23DB5BC3,0xB34C2234};
+/* OTAA para*/
+uint8_t DevEui[] = { 0x60, 0x81, 0xF9, 0xF1, 0x26, 0x6B, 0x13, 0x9F };
+uint8_t AppEui[] = { 0x60, 0x81, 0xF9, 0xD2, 0xCE, 0x19, 0xE0, 0x5F };
+uint8_t AppKey[] = { 0xE2, 0xAD, 0x07, 0xC4, 0x78, 0xFF, 0x60, 0xBA, 0xC0, 0x29, 0x5D, 0xDE, 0xCC, 0x06, 0xBA, 0xC7 };
+
+/* ABP para*/
+uint8_t NwkSKey[] = { 0x15, 0xb1, 0xd0, 0xef, 0xa4, 0x63, 0xdf, 0xbe, 0x3d, 0x11, 0x18, 0x1e, 0x1e, 0xc7, 0xda,0x85 };
+uint8_t AppSKey[] = { 0xd7, 0x2c, 0x78, 0x75, 0x8c, 0xdc, 0xca, 0xbf, 0x55, 0xee, 0x4a, 0x77, 0x8d, 0x16, 0xef,0x67 };
+uint32_t DevAddr =  ( uint32_t )0x007e6ae1;
+
+/*LoraWan Class, Class A and Class C are supported*/
+DeviceClass_t  loraWanClass = CLASS_A;
+
+/*the application data transmission duty cycle.  value in [ms].*/
+uint32_t appTxDutyCycle = 15000;
+
+/*OTAA or ABP*/
+bool overTheAirActivation = true;
+
+/*ADR enable*/
+bool loraWanAdr =false;
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
+bool isTxConfirmed = true;
+
+/*LoraWan channelsmask, default channels 0-7*/
+uint16_t userChannelsMask[6]={ 0xFF00,0x0000,0x0000,0x0000,0x0000,0x0000 };
+
+
+/* Application port */
+uint8_t appPort = 2;
+
+/*!
+* Number of trials to transmit the frame, if the LoRaMAC layer did not
+* receive an acknowledgment. The MAC performs a datarate adaptation,
+* according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
+* to the following table:
+*
+* Transmission nb | Data Rate
+* ----------------|-----------
+* 1 (first)       | DR
+* 2               | DR
+* 3               | max(DR-1,0)
+* 4               | max(DR-1,0)
+* 5               | max(DR-2,0)
+* 6               | max(DR-2,0)
+* 7               | max(DR-3,0)
+* 8               | max(DR-3,0)
+*
+* Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
+* the datarate, in case the LoRaMAC layer did not receive an acknowledgment
+*/
+uint8_t confirmedNbTrials = 8;
+
+/*LoraWan debug level, select in arduino IDE tools.
+* None : print basic info.
+* Freq : print Tx and Rx freq, DR info.
+* Freq && DIO : print Tx and Rx freq, DR, DIO0 interrupt and DIO1 interrupt info.
+* Freq && DIO && PW: print Tx and Rx freq, DR, DIO0 interrupt, DIO1 interrupt, MCU sleep and MCU wake info.
+*/
+uint8_t debugLevel = LoRaWAN_DEBUG_LEVEL;
+
+/*LoraWan region, select in arduino IDE tools*/
+LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
+
+const size_t capacity = JSON_OBJECT_SIZE(1) + 10;
+DynamicJsonDocument payload(capacity);
+static void prepareTxFrame( uint8_t port )
+{
+//  float temp = (float)(10);
+//  unsigned char *puc;
+//  puc = (unsigned char *)(&temp);
+//  
+//    appDataSize = 4;//AppDataSize max value is 64
+//     appData[0] = 0x00;
+//    appData[1] = 0x01;
+//    appData[2] = 0x02;
+//    appData[3] = 0x03;
+  serializeMsgPack(payload, appData);
+  appDataSize = measureMsgPack(payload);
+    
+}
+
+// Add your initialization code here
+void setup()
+{
+  if(mcuStarted==0)
+  {
+    LoRaWAN.displayMcuInit();
+  }
+  Serial.begin(115200);
+  while (!Serial);
+  SPI.begin(SCK,MISO,MOSI,SS);
+  
+  payload["ms"] = 100;
+  serializeJsonPretty(payload, Serial);
+  
+  Mcu.init(SS,RST_LoRa,DIO0,DIO1,license);
+  deviceState = DEVICE_STATE_INIT;
+ 
+  
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+  switch( deviceState )
+  {
+    case DEVICE_STATE_INIT:
+    {
+      LoRaWAN.init(loraWanClass,loraWanRegion);
+      break;
+    }
+    case DEVICE_STATE_JOIN:
+    {
+      LoRaWAN.displayJoining();
+      LoRaWAN.join();
+      break;
+    }
+    case DEVICE_STATE_SEND:
+    {
+      LoRaWAN.displaySending();
+      prepareTxFrame( appPort );
+      LoRaWAN.send(loraWanClass);
+      deviceState = DEVICE_STATE_CYCLE;
+      break;
+    }
+    case DEVICE_STATE_CYCLE:
+    {
+      // Schedule next packet transmission
+      txDutyCycleTime = appTxDutyCycle + randr( -APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND );
+      LoRaWAN.cycle(txDutyCycleTime);
+      deviceState = DEVICE_STATE_SLEEP;
+      break;
+    }
+    case DEVICE_STATE_SLEEP:
+    {
+      LoRaWAN.displayAck();
+      LoRaWAN.sleep(loraWanClass,debugLevel);
+      break;
+    }
+    default:
+    {
+      deviceState = DEVICE_STATE_INIT;
+      break;
+    }
+  }
+}
